f57c3be4a836ea65f66c4a3c7457bf29cc3b77ec
Alexander Winberg <alewinbe@gmail.com>
2021-12-03 16:13:23 +0200

    refactored day 1

--- Day1.py
+++ Day1.py
@@ -1,20 +1,26 @@
-with open("Day1_input.txt", "r") as f:
-    data = f.read()
+def part1(list: list):
+    increase = 0    
+    
+    for count, i in enumerate(list):
+        if count > 0 and int(i) > int(list[count-1]):
+           increase += 1  
+           
+    return increase
 
-list = data.split('\n')
-# increase = 0
+def part2(list: list):
+    listofsums = []
+    
+    for count, i in enumerate(list):
+        if count >= 2:
+            listofsums.append(int(i) + int(list[count-1]) + int(list[count-2]))
 
-# for count, i in enumerate(list):
-#     if count > 0 and int(i) > int(list[count-1]):
-#        increase += 1 
-       
-# print(increase)
+    increased = [i for count, i in enumerate(listofsums) if i > listofsums[count-1] and count > 0]
+    return len(increased)
 
-listofsums = []
+if __name__ == '__main__':
+    with open("Day1_input.txt", "r") as f:
+        data = f.read()
 
-for count, i in enumerate(list):
-    if count >= 2:
-        listofsums.append(int(i) + int(list[count-1]) + int(list[count-2]))
+    list = data.split('\n')
 
-increased = [i for count, i in enumerate(listofsums) if i > listofsums[count-1] and count > 0]
-print(len(increased))
+    print(f'Part 1: {part1(list)}\nPart 2: {part2(list)}')
